4e2458e086d015fe2c705e2c17ca8ce12986fc86
peijungwu0302-Wu <peijungwu0302@gmail.com>
2026-09-09 01:36:12 +0800

    Limit Swift Playgrounds manifest to WhisperKit

--- Package@swift-6.2.swift
+++ Package@swift-6.2.swift
@@ -14,49 +14,12 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "ArgmaxOSS",
-            targets: ["ArgmaxOSS"]
-        ),
-        .library(
             name: "WhisperKit",
             targets: ["WhisperKit"]
         ),
-        .library(
-            name: "TTSKit",
-            targets: ["TTSKit"]
-        ),
-        .library(
-            name: "SpeakerKit",
-            targets: ["SpeakerKit"]
-        ),
-        .executable(
-            name: "argmax-cli",
-            targets: ["ArgmaxCLI"]
-        ),
-        .executable(
-            name: "whisperkit-cli",
-            targets: ["ArgmaxCLI"]
-        ),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
-    ] + (isServerEnabled() ? [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.1"),
-        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.10.2"),
-        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.8.2"),
-        .package(url: "https://github.com/swift-server/swift-openapi-vapor", from: "1.0.1"),
-    ] : []),
+    dependencies: [],
     targets: [
-        .target(
-            name: "ArgmaxOSS",
-            dependencies: [
-                "ArgmaxCore",
-                "WhisperKit",
-                "TTSKit",
-                "SpeakerKit",
-            ],
-            swiftSettings: swiftSettings()
-        ),
         .target(
             name: "ArgmaxCore",
             swiftSettings: swiftSettings()
@@ -68,83 +31,9 @@ let package = Package(
             ],
             swiftSettings: swiftSettings()
         ),
-        .target(
-            name: "TTSKit",
-            dependencies: [
-                "ArgmaxCore",
-            ],
-            swiftSettings: swiftSettings()
-        ),
-        .target(
-            name: "SpeakerKit",
-            dependencies: [
-                "ArgmaxCore",
-                "WhisperKit",
-            ],
-            swiftSettings: swiftSettings()
-        ),
-        .testTarget(
-            name: "ArgmaxCoreTests",
-            dependencies: [
-                "ArgmaxCore",
-            ],
-            resources: [
-                .process("External/Resources"),
-            ],
-            swiftSettings: swiftSettings(libraryEvolution: false)
-        ),
-        .testTarget(
-            name: "WhisperKitTests",
-            dependencies: [
-                "WhisperKit",
-            ],
-            exclude: ["UnitTestsPlan.xctestplan"],
-            resources: [
-                .process("Resources"),
-            ],
-            swiftSettings: swiftSettings(libraryEvolution: false)
-        ),
-        .testTarget(
-            name: "TTSKitTests",
-            dependencies: [
-                "TTSKit"
-            ],
-            swiftSettings: swiftSettings(libraryEvolution: false)
-        ),
-        .testTarget(
-            name: "SpeakerKitTests",
-            dependencies: [
-                "SpeakerKit",
-                "WhisperKit",
-            ],
-            resources: [
-                .process("Resources"),
-            ],
-            swiftSettings: swiftSettings(libraryEvolution: false)
-        ),
-        .executableTarget(
-            name: "ArgmaxCLI",
-            dependencies: [
-                "WhisperKit",
-                "TTSKit",
-                "SpeakerKit",
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ] + (isServerEnabled() ? [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
-            ] : []),
-            path: "Sources/ArgmaxCLI",
-            exclude: (isServerEnabled() ? [] : ["Server"]),
-            swiftSettings: swiftSettings(libraryEvolution: false) + (isServerEnabled() ? [.define("BUILD_SERVER_CLI")] : [])
-        )
     ],
     swiftLanguageModes: [.v6]
 )
-
-func isServerEnabled() -> Bool {
-    false
-}
 
 func swiftSettings(libraryEvolution: Bool = true) -> [SwiftSetting] {
     // Opt-in to Swift 6.2's "Approachable Concurrency" upcoming features.
